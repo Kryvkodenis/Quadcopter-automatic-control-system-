32dd164c6847b19c407a77e52f6711017624981a
Kryvkodenis <49016304+Kryvkodenis@users.noreply.github.com>
2020-08-30 23:47:03 +1000

    Update Manager.py

--- simulation/Manager.py
+++ simulation/Manager.py
@@ -10,13 +10,13 @@ import cv2
 import numpy as np
 import sys, time
 
-comands = {'start system': 1, 'send_path': 2, 'send_GPS': 3,
+commands = {'start system': 1, 'send_path': 2, 'send_GPS': 3,
            'send_accs': 4, 'send_gyro': 5, 'send_video': 6,
            'send_target_pos': 7, 'send_H_sens': 8, 'sensor_ready': 9,
            'path_ready': 10, 'send_orientation_drone': 11, 'send_orientation_target': 12,
            'send_U': 13, 'controll_ready': 14, 'user_path': 15, 'position drone': 16, 'pause': 17,
            'stop_sim': 18, 'Pid_1_set': 19, 'Pid_2_set': 20, 'Pid_3_set': 21,
-           'Pid_4_set': 22, 'continue': 23}
+           'Pid_4_set': 22, 'continue': 23, 'start_video': 24, 'Pid_5_set': 25, 'Pid_6_set': 26, 'velocity': 27}
 
 IP = {'manager': '127.0.0.1', 'obstacle avoidance': '127.0.0.1',
       'Path generator': '127.0.0.1', 'navigation system': '127.0.0.1',
@@ -48,47 +48,42 @@ class massage(initialization):
         super().__init__(IP, ports)
 
     def get_massage(self):
-        massage = self.local_socket.recv(2**15)
+        massage = self.local_socket.recv(64)
         code = int.from_bytes(massage[0:2], "big")
         print(code)
         if code == 2 or code == 3 or code == 4 or code == 5 or code == 7 or code == 16 \
-                or code == 11 or code == 12 or code == 21 or code == 22:
-            massage = list(struct.unpack('>fff', massage[2::]))
+                or code == 11 or code == 12 or code == 22 \
+                or code == 25 or code == 26 or code == 27 or code == 20 or code == 21:
+            massage = list(struct.unpack('>3f', massage[2::]))
             return code, (*massage)
         elif code == 8:
             massage = list(struct.unpack('>?6f', massage[2::]))
             return code, (*massage)
-        elif code == 1 or code == 17 or code == 18 or code == 23 or code == 9 or code == 10 or code == 14:
+        elif code == 1 or code == 17 or code == 18 or code == 23 or code == 9 or code == 10 or code == 14 or code == 24:
             return [code]
-        elif code == 13 or code == 19 or code == 20:
+        elif code == 13 or code == 19:
             massage = list(struct.unpack('>4f',  massage[2::]))
             return code, (*massage)
-        elif code == 6:
-            self.local_socket.sendto(massage, (IP['inteface'], ports['inteface']))
-            return [6]
         elif code == 15:
             pass
 
     def send_massage(self, code, data, IP, port, res = None):
         if code == 2 or code == 3 or code == 4 or code == 5 or code == 7 \
-                or code == 16 or code == 11 or code == 12 or code == 21 or code == 22:
-            format = '>hfff'
+                or code == 16 or code == 11 or code == 12 or code == 22\
+                or code == 25 or code == 26 or code == 27 or code == 20 or code == 21:
+            format = '>h3f'
             massage = struct.pack(format, code, *data)
             self.local_socket.sendto(massage, (IP, port))
         elif code == 8:
             format = '>h?6f'
             massage = struct.pack(format, code, data[0], *data[1], *data[2])
             self.local_socket.sendto(massage, (IP, port))
-        elif code == 1 or code == 17 or code == 18 or code == 23 or code == 9 or code == 10 or code == 14:
+        elif code == 1 or code == 17 or code == 18 or code == 23 or code == 9 or code == 10 or code == 14 or code == 24:
             format = '>h'
             massage = struct.pack(format, code)
             self.local_socket.sendto(massage, (IP, port))
-        elif code == 6:
-            format = '>hhh' + str(res[0]*res[1]*3) + 'h'
-            data = np.array(data).flatten()
-            massage = struct.pack(format, code, res[0], res[1], *data)
-            self.local_socket.sendto(massage, (IP, port))
-        elif code == 13 or code == 19 or code == 20:
+        elif code == 13 or code == 19:
+            print(data)
             format = '>h4f'
             massage = struct.pack(format, code, *data)
             self.local_socket.sendto(massage, (IP, port))
@@ -105,18 +100,15 @@ massage_manager = massage(IP['manager'], ports['manager'])
 
 
 # посылвем всем модулям команду о готовности к работе do with outstanding method
-sensor_ready, controll_ready = [False for x in range(2)]
+sensor_ready = False
 
-while not (sensor_ready and controll_ready):
+while not sensor_ready:
     if not sensor_ready:
-        massage_manager.send_massage(comands['start system'], None, IP['sensor module'], ports['sensor module'])
-    if not controll_ready:
-        massage_manager.send_massage(comands['start system'], None, IP['stabilization'], ports['stabilization'])
+        massage_manager.send_massage(commands['start system'], None, IP['sensor module'], ports['sensor module'])
     code = massage_manager.get_massage()
     if code[0] == 9:
         sensor_ready = True
-    elif code[0] == 14:
-        controll_ready = True
+
 
 
 start_sim = True
@@ -124,51 +116,20 @@ while True:
 
     while start_sim:
         input_data = massage_manager.get_massage()
-        if input_data[0] == 17:
-            start_sim = False
-            massage_manager.send_massage(comands['pause'], 1, IP['stabilization'], ports['stabilization'])
-        elif input_data[0] == 18:
-            start_sim = False
-            massage_manager.send_massage(comands['stop_sim'], 1, IP['stabilization'], ports['stabilization'])
 
-        elif input_data[0] == 7 or input_data[0] == 11 or input_data[0] == 12 or input_data[0] == 16 or input_data[0] ==19 \
-                or input_data[0] == 20 or input_data[0] == 21 or input_data[0] == 22:
-            massage_manager.send_massage(input_data[0], input_data[1::], IP['stabilization'], ports['stabilization'])
-        elif input_data[0] == 13:
-
-            massage_manager.send_massage(comands['send_U'], input_data[1::], IP['sensor module'], ports['sensor module'])
+        if input_data[0] == 24:
+            massage_manager.send_massage(commands['start_video'], 1, IP['sensor module'], ports['sensor module'])
         elif input_data[0] == 3 or input_data[0] == 11:
             massage_manager.send_massage(input_data[0], input_data[1::], IP['inteface'], ports['inteface'])
+        elif input_data[0] == 20 or input_data[0] == 21 or input_data[0] == 22 or \
+                input_data[0] == 25 or input_data[0] == 26 or input_data[0] == 19:
+            massage_manager.send_massage(input_data[0], input_data[1::], IP['sensor module'], ports['sensor module'])
+
 
     input_data = massage_manager.get_massage()
     if input_data[0] == 1:
         start_sim = True
-        massage_manager.send_massage(comands['continue'], 1, IP['stabilization'], ports['stabilization'])
-        massage_manager.send_massage(comands['continue'], 1, IP['sensor module'], ports['sensor module'])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''if res == sim.simx_return_ok:
-    im = array.array('b', image)
-    im.reverse()
-    im = img.frombuffer('RGB', resolution, byte(im), 'raw', 'RGB', 0, 1)
-    image_cv = np.asarray(im)
-    cv2.flip(image_cv, 1, image_cv)
-    cv2.imshow('im', image_cv)
-    k = cv2.waitKey(1)
-else:
-    print('НЕ РАБОТАЕТ!!')'''
+        massage_manager.send_massage(commands['continue'], 1, IP['sensor module'], ports['sensor module'])
 
 
 
